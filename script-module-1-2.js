0ea9ddc66aaa95c01e10f6f2d170ab1262638955
Maria Belik <mashabelik@gmail.com>
2021-04-01 17:01:53 +0300

    update

--- script-module-1-2.js
+++ script-module-1-2.js
@@ -338,13 +338,13 @@
 //==найти=самое=маленькое=число=в=массиве=================================================================
 // const numbers = [51, 18, 13, 24, 7, 85, 19];
 // let smallestNumber = numbers[0];
-//
+
 // for (const number of numbers) {
 //   if (number < smallestNumber) {
 //     smallestNumber = number;
 //   }
 // }
-//
+
 // console.log(`${smallestNumber} is a smallest number`);
 
 //==соединение=двух=массивов====================================================================
---
+++ script-module-5.js
@@ -0,0 +1,312 @@
+//=5=19============================================================
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Пиши код ниже этой строки  
+//     blacklistedEmails = [];
+
+//     static AccessLevel = {
+//         BASIC: 'basic',
+//         SUPERUSER: 'superuser'
+//     };
+
+//     accessLevel;
+
+//     constructor({ email, accessLevel }) {
+//         super(email);
+//         this.accessLevel = accessLevel;
+//     }
+//     blacklist(email) {
+//         this.blacklistedEmails.push(email);
+//     }
+//     isBlacklisted(email) {
+//         if (this.blacklistedEmails.includes(email)) {
+//             return true;
+//         };
+//         return false;
+//     }
+//   // Пиши код выше этой строки
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER
+// });
+
+// console.log(mango.email); // mango@mail.com
+// console.log(mango.accessLevel); // superuser
+// mango.blacklist('poly@mail.com');
+// console.log(mango.blacklistedEmails); // 'poly@mail.com'
+// console.log(mango.isBlacklisted('mango@mail.com')); //  false
+// console.log(mango.isBlacklisted('poly@mail.com')); // true 
+
+// //==5=15=============================================================
+
+// class Car {
+//   // Пиши код ниже этой строки
+//   static MAX_PRICE = 50000;
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//     set price(newPrice) {
+//         if (newPrice < Car.MAX_PRICE) {
+//             this.#price = newPrice;            
+//         }
+//         this.#price;            
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// const audi = new Car({price: 35000});
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
+//==5=13?=============================================================
+// function StringBuilder(baseValue) {
+//   this.value = baseValue;
+// }
+
+// StringBuilder.prototype.getValue = function () {
+//   return this.value;
+// };
+
+// StringBuilder.prototype.padEnd = function (str) {
+//   this.value += str;
+// };
+
+// StringBuilder.prototype.padStart = function (str) {
+//   this.value = str + this.value;
+// };
+
+// StringBuilder.prototype.padBoth = function (str) {
+//   this.padStart(str);
+//   this.padEnd(str);
+// };
+
+//--рефакторинг--------------------------------------------------
+// class StringBuilder {
+//     #value;
+
+//     constructor(baseValue) {
+//         this.#value = baseValue;
+//     }
+
+//     getValue() {
+//         return this.#value;
+//     }
+
+//     padEnd(str) {
+//         this.#value += str;
+//     }
+
+//     padStart(str) {
+//         this.#value = str + this.#value;
+//     }
+
+//     padBoth(str) {
+//         this.padStart(str);
+//         this.padEnd(str);
+//     }
+
+// }
+
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+//===================================================================
+// const objC = {
+//     z: 5,
+// };
+// console.log("Obj C", objC);
+
+// const objB = Object.create(objC)
+// objB.y = 4;
+// console.log("Obj B", objB);
+
+// const objA = Object.create(objB)
+// objA.x = 3;
+// console.log("Obj A", objA);
+
+// console.log("посмотреть цепочку", objA);
+
+// objA.z = 8;
+// console.log("значение Z запишется в objA, а не перезапишется z в objC", objA);
+
+
+
+//==5=12===========================================================
+// class Storage {
+//     #items;
+//     constructor(items) {
+//         this.#items = items;
+//     }
+//     getItems() {
+//         return this.#items;
+//     }
+//     addItem(newItem) {
+//         this.#items.push(newItem);
+//     }
+//     removeItem (item) {
+//         const itemIndex = this.#items.indexOf(item);
+//         this.#items.splice(itemIndex, 1);
+//     }
+// }
+
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+
+//==5=11========================================================
+// class Car {
+// #brand;
+
+//     constructor({ brand, model, price }) {
+//         this.#brand = brand;
+//         this.model = model;
+//         this.price = price;
+//     }
+    
+//     getBrand() {
+//         return this.#brand;
+//     }
+
+//     changeBrand(newBrand) {
+//         this.#brand = newBrand;
+//     }
+// }
+
+//==5=10=========================================================
+// class Car {
+//     constructor({ brand, model, price }) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.price = price;
+//     }
+    
+//     getPrice() {
+//         return this.price
+//     }
+
+//     changePrice(newPrice) {
+//         this.price = newPrice;
+//     }
+//   }
+
+//==5=9==========================================================
+// function Car({ brand, model, price }) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+// class Car {
+//     brand;
+//     model;
+//     price;
+
+//     constructor({ brand, model, price }) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.price = price;
+//     }
+// }
+
+//==5=7===========================================================
+// function StringBuilder(baseValue) {
+//     this.value = baseValue;
+// }
+
+// StringBuilder.prototype.getValue = function() {
+//   return this.value;
+// };
+
+// StringBuilder.prototype.padEnd = function(str) {
+//   return this.value = this.value + str;
+// };
+
+// StringBuilder.prototype.padStart = function(str) {
+//   return this.value = str + this.value;
+// };
+
+// StringBuilder.prototype.padBoth = function (str) {
+//   return this.value = str + this.value + str;
+// };
+
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+
+//==5=6==========================================================
+// function Storage(items) {
+//     this.items = items;
+// };
+
+// Storage.prototype.getItems = function() {
+//   return this.items;
+// };
+
+// Storage.prototype.addItem = function(newItem) {
+//     return this.items.push(newItem);
+// };
+
+// Storage.prototype.removeItem = function (item) {
+//     for (const itm of this.items) {
+//         if (itm === item)
+//         this.items.splice(this.items.indexOf(itm), 1);
+//     }
+//     return this.items;
+// };
+
+
+// // Пиши код выше этой строки
+// const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem('Дроид');
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
